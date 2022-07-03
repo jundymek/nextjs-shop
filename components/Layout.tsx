4c5a1c75cfd83be45171ee72f9a9266a97851ba6
jundymek <jundymek@gmail.com>
2022-07-03 17:40:12 +0200

    feat: add next-seo to ProductDetails component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
@@ -5,6 +6,11 @@ import { Header } from "./Header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <Head>
+        <title>Nasz Sklep</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Nasz sklep" />
+      </Head>
       <Header />
       <main className="container mx-auto flex-grow flex flex-col items-center justify-center"> {children} </main>
       <Footer />
--- components/Product.tsx
+++ components/Product.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { Rating } from "./Rating";
 import ReactMarkdown from "react-markdown";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 export interface ProductDetails {
   id: number;
@@ -20,85 +22,104 @@ interface ProductDetailsProps {
 export const ProductDetails = ({ data }: ProductDetailsProps) => {
   const router = useRouter();
   return (
-    <section className="text-gray-600 body-font overflow-hidden">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="lg:w-4/5 mx-auto flex flex-col md:flex-row items-center bg-white shadow-sm relative">
-          <Image
-            width={500}
-            height={500}
-            className="rounded object-contain"
-            src={data.thumbnailUrl}
-            alt={data.thumbnailAlt}
-          />
-          <div className="lg:w-1/2 w-full p-6 lg:px-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
-            <div className="flex mb-4">
-              <Rating rating={data.rating} />
-            </div>
-            <article className="prose">
-              <ReactMarkdown>{data.longDescription}</ReactMarkdown>
-            </article>
-            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-              <div className="flex">
-                <span className="mr-3">Color</span>
-                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+    <>
+      <NextSeo
+        title={data.title}
+        description={data.description}
+        canonical={`https://nextjs-shop-eight.vercel.app/products/${data.id}`}
+        openGraph={{
+          url: `https://nextjs-shop-eight.vercel.app/products/${data.id}`,
+          title: data.title,
+          description: data.description,
+          images: [
+            {
+              url: data.thumbnailUrl,
+              alt: data.thumbnailAlt,
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "Next sklep",
+        }}
+      />
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-col md:flex-row items-center bg-white shadow-sm relative">
+            <Image
+              width={500}
+              height={500}
+              className="rounded object-contain"
+              src={data.thumbnailUrl}
+              alt={data.thumbnailAlt}
+            />
+            <div className="lg:w-1/2 w-full p-6 lg:px-10 lg:py-6 mt-6 lg:mt-0">
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{data.title}</h1>
+              <div className="flex mb-4">
+                <Rating rating={data.rating} />
               </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
-                <div className="relative">
-                  <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                  </select>
-                  <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-4 h-4"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M6 9l6 6 6-6"></path>
-                    </svg>
-                  </span>
+              <article className="prose">
+                <ReactMarkdown>{data.longDescription}</ReactMarkdown>
+              </article>
+              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                <div className="flex">
+                  <span className="mr-3">Color</span>
+                  <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                  <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                  <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                </div>
+                <div className="flex ml-6 items-center">
+                  <span className="mr-3">Size</span>
+                  <div className="relative">
+                    <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                      <option>SM</option>
+                      <option>M</option>
+                      <option>L</option>
+                      <option>XL</option>
+                    </select>
+                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-4 h-4"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M6 9l6 6 6-6"></path>
+                      </svg>
+                    </span>
+                  </div>
                 </div>
               </div>
+              <div className="flex">
+                <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
+                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  Button
+                </button>
+                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              </div>
             </div>
-            <div className="flex">
-              <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
-              <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
-              </button>
-              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
-            </div>
+            <button
+              type="button"
+              onClick={() => router.back()}
+              className="absolute -bottom-20 right-0  flex ml-auto text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded"
+            >
+              Back to products
+            </button>
           </div>
-          <button
-            type="button"
-            onClick={() => router.back()}
-            className="absolute -bottom-20 right-0  flex ml-auto text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded"
-          >
-            Back to products
-          </button>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
